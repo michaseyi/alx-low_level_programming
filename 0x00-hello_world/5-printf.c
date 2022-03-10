0810f2d52e97996afa56ace6e4305f624ba4693c
Adewole Michael <michaseyi@gmail.com>
2022-03-10 23:32:49 +0100

    Intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main()
+/*
+ * main - Entry point
+ * Description - prints "with proper grammer, but the outcome is a piece of art,"
+ * Return :0
+ */
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
