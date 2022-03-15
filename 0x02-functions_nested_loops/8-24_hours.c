439bf01bbb4399fdbf097023cf8ac056465e209f
Adewole Michael <michaseyi@gmail.com>
2022-03-16 00:59:45 +0100

    funtions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,46 @@
 #include "main.h"
 
 /**
- * jack_bauer - Entry point
- * Description : prints out all minutes in a day
- * Return: (void)
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
  */
-
 void jack_bauer(void)
 {
-	int a, b, c, d, i;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	d = 0;
-	for (i = 0; i < 1440; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		_putchar(a + '0');
-		_putchar(b + '0');
-		_putchar(':');
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar('\n');
-		if (d == 9) 
+		if (hours_tens == '2')
 		{
-			d = 0;
-			if (c == 5)
-			{
-				c = 0;
-				if (b == 9)
-				{
-					b = 0;
-					a++;
-				} else
-				{
-					b++;
-				}
-			} else 
-			{
-				c++;
-			}
-		} else 
-		{
-			d++;
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
-
