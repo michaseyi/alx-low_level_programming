b214b2417c5ec916598d8ec2ad679e11cd766f6d
Adewole Michael <michaseyi@gmail.com>
2022-03-16 01:09:36 +0100

    funtions

--- 0x02-functions_nested_loops/table.js
+++ 0x02-functions_nested_loops/table.js
@@ -1,12 +1,15 @@
 function timesTable() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 16; i++) {
     let count = 0;
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j < 16; j++) {
       if (j === 0) {
         process.stdout.write(`${count}`);
       } else {
         process.stdout.write(`,`);
         if (count < 10) {
+          process.stdout.write(" ");
+        }
+        if (count < 100) {
           process.stdout.write(" ");
         }
         process.stdout.write(" ");
