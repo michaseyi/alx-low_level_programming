3b3caa8c685a677323733711f8f610d9de840dbe
Adewole Michael <michaseyi@gmail.com>
2022-03-17 22:43:40 +0100

    print most numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+
+/**
+ * mul - multiplies two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints out No from 1 - 9 execpt
+ * 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '4' || i == '2)
+			continue;
+		_putchar(i);
+	}
+	_putchar(i);
+}
