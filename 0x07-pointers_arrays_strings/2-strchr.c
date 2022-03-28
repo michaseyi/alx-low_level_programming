a763d73842af863a4c8ec716cebc76aad518a9e5
Adewole Michael <michaseyi@gmail.com>
2022-03-28 23:07:53 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - checks for the occurence of character c
+ * @s: string to be searched
+ * @c: character to be searched for
+ * Return: pointer to s if c is in s or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c && s[i])
+		;
+	if (s[i])
+	{
+		return (s + i);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 char _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
