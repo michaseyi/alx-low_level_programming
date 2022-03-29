1447aa82bac83b861d940ce71541efa1d62e28c6
Adewole Michael <michaseyi@gmail.com>
2022-03-29 01:23:17 +0100

    double pinter, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: first input
+ * @to: value to be set
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
