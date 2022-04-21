4b858afbe10d91fad49fedbed32411a041396af1
Adewole Michael <michaseyi@gmail.com>
2022-04-21 21:41:09 +0100

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 
-
 /**
  * free_list - Frees a list_t list
  * @head: Head of the list
@@ -9,11 +8,11 @@
 
 void free_list(list_t *head)
 {
-	if (head->next != NULL)
+	if (head && head->next != NULL)
 		free_list(head->next);
-	if (head == NULL)
-		return;
-	if (head->str != NULL)
+	if (head != NULL)
+	{
 		free(head->str);
-	free(head);
+		free(head);
+	}
 }
