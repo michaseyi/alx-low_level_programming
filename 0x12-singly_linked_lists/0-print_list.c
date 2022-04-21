b6199f2068121b5f861a58df56df2febfbdeb9e0
Adewole Michael <michaseyi@gmail.com>
2022-04-21 21:14:51 +0100

    add_node_end.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all elements of a list_t
+ * @h: Input data of type list_t
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	size_t i = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
