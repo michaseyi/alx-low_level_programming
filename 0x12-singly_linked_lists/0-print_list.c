c140a7810b74c810f93a473dbcaa0d602f12e4ca
Adewole Michael <michaseyi@gmail.com>
2022-04-21 23:09:41 +0100

    hello holberton

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	const list_t *current = h;
+	size_t i = 0;
 
-	while (h)
+	while (current)
 	{
-		if (h->str == NULL)
+		if (current->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		i++;
 	}
-
-	return (nodes);
+	return (i);
 }
