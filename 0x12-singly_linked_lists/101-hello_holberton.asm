e076b0773b5476c8c0c9575ceaac19fceaceb213
Adewole Michael <michaseyi@gmail.com>
2022-04-21 22:54:24 +0100

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .text
+    default rel
+    extern printf
+    global main
+
+main:
+    push rbp
+    mov rdi, fmt
+    mov rsi, message
+    mov rax, 0
+
+    call printf
+
+    pop rbp
+    
+    mov rax, 0
+    ret
+
+section.data
+    message: db "Hello, Holberton", 0
+    fmt: db "%s", 10, 0
