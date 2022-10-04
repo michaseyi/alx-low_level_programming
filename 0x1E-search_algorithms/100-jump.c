1965d05d6db44997f0bba489ba09c28701ff8f2c
Adewole Michael <michaseyi@gmail.com>
2022-10-04 15:12:16 +0100

    Fixed task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump_size = (size_t)round(sqrt(size));
+	size_t jump_size = (size_t)sqrt(size);
 	size_t start = 0, end = jump_size, i;
 
 	if (!array || !size)
