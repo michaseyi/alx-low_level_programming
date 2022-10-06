5eac681bdae0a003936936d0115d817b266b8475
Adewole Michael <michaseyi@gmail.com>
2022-10-06 23:42:20 +0100

    Added task 12

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * advance - advances a pointer forward
+ * @current: is the pointer to advance
+ * @count: is how much to advance by
+ * Return: is a pointer to the new location of NULL if the end
+ * was reached
+ */
+listint_t *advance(listint_t *current, size_t count)
+{
+	while (count)
+	{
+		if (!current)
+			break;
+		current = current->next;
+		count--;
+	}
+	return (current);
+}
+
+/**
+ * jump_list - searches for a value in sorted list of integers using the jump
+ * search algorithm
+ * @list: is a pointer to the  head of the list to search in
+ * @size: is the number of nodes  in list
+ * @value: is the value to search for
+ * Return: is a pointer to the first node where value is located
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t jump_size = (size_t)sqrt(size);
+	listint_t *start = list, *end = advance(list, jump_size);
+
+	if (!list)
+		return (NULL);
+	while (start->index < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", start->index, start->n);
+		if (!end || end->n >= value)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				   start->index, end ? end->index : size - 1);
+			while (1)
+			{
+				if (!start)
+					return (NULL);
+				printf("Value checked array[%lu] = [%d]\n", start->index, start->n);
+				if (start->n == value)
+					return (start);
+				if (start == end)
+				{
+					return (NULL);
+				}
+				start = start->next;
+			}
+		}
+		start = advance(start, jump_size);
+		end = advance(end, jump_size);
+	}
+	return (NULL);
+}
